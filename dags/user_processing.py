9255105d519967850a5754c043196c7bd12afc1b
yingxie24 <cheyannexie@gmail.com>
2025-06-29 10:11:12 +0100

    Add new tasks for Sensor and PythonOperator

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,9 +1,25 @@
-from airflow.sdk import dag 
+from airflow.sdk import dag, task
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.sdk.bases.sensor import PokeReturnValue
+from airflow.providers.standard.operators.python import PythonOperator
+
+
+def _extract_user(ti):
+    # fake_user = ti.xcom_pull(task_ids="check_API_exists")
+    import requests
+    response = requests.get("https://raw.githubusercontent.com/marclamberti/datasets/refs/heads/main/fakeuser.json")
+    fake_user = response.json()
+    return {
+        "id": fake_user["id"],
+        "firstname": fake_user["personalInfo"]["firstName"],
+        "lastname": fake_user["personalInfo"]["lastName"],
+        "email": fake_user["personalInfo"]["email"],
+    } 
 
 @dag
 def user_processing(): 
     
+    # Task 1: Connect to PostgreSQL and create a table
     create_table = SQLExecuteQueryOperator(
         task_id="create_table",
         conn_id="postgres",
@@ -17,5 +33,32 @@ def user_processing():
             );
             """
     )
+
+    # Task 2: Check if the API exists and fetch content
+    @task.sensor(poke_interval=30, timeout=300)
+    def check_API_exists() -> PokeReturnValue:
+        import requests
+        response = requests.get("https://raw.githubusercontent.com/marclamberti/datasets/refs/heads/main/fakeuser.json")
+        print(f"Status code: {response.status_code}")
+
+        if response.status_code == 200:
+            condition = True
+            fake_user = response.json()
+
+        else:
+            condition = False
+            fake_user = None
+
+        return PokeReturnValue(is_done=condition, xcom_value=fake_user)
+    
+    check_API_exists()
+
+    # Task 3: Parsing the JSON data pulled from the API sensor
+
+    extract_user = PythonOperator(
+        task_id = "extract_user",
+        python_callable = _extract_user,
+    )
+
 
 user_processing()
